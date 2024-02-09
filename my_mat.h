92cba71383a65e345098fbbaa4c8c8e35cda88ac
hila1999 <118314052+hila1999@users.noreply.github.com>
2024-02-09 15:25:46 +0200

    Update my_mat.h

--- my_mat.h
+++ my_mat.h
@@ -1,7 +1,17 @@
-#ifndef _MY_MAT_
-#define _MY_MAT_
-int * buildArr();
-boolean ifPathIsExists(int i,int j);
-int shortPath(int i,int j);
-void selectItems(float weights[], float values[], char *strings[], int numStrings);
+#ifndef _MY_MAT_H
+#define _MY_MAT_H
+#define V 10
+#define MATRIX_SIZE 10
+#define INT_MAX 2147483647
+#define LEN 5
+#define W 20
+#define CHAR_MAX 20
+//1
+void getMatFromUser(int**,int);
+//2
+int isPath(int**,int,int,int);
+
+int findShortestPath(int **,int,int,int);
+void setItems(char* items,int* value,int* weight, int len);
+int knapSack( int weights[], int values[] , int selected_bool[]);
 #endif
